bcaffc73468784c84d4603453f2f652ce2782436
satya6371 <104180779+satya6371@users.noreply.github.com>
2023-09-14 19:27:03 +0530

    second commit

--- backend/controller/userController.js
+++ backend/controller/userController.js
@@ -3,8 +3,6 @@ const bcrypt = require('bcryptjs')
 const asyncHandler = require('express-async-handler')
 const User = require('../models/userModel.js')
 
-
-
 //     @Register new user
 //     @POST/api/users
 //     @public
@@ -18,7 +16,29 @@ const registerUser = asyncHandler(async(req,res) => {
     // Check  if user exists
     const userExists = await User.findOne({email})
     if(userExists){
-        
+        res.status(400)
+        throw new Error("User already exists")
+    }
+    // Hass the password
+    const salt =await bcrypt.genSalt(10)
+    const hashedPassword = await bcrypt.hash(password,salt)
+    // Create user
+    const user = await User.create({
+        name,
+        email,
+        password:hashedPassword
+    })
+    if (user) {
+        res.status(201).json({
+            _id:user .id,
+            name:user.name,
+            emali:user.email,
+            token:generateToken(user._id)
+        })
+    }
+    else{
+        res.status(400)
+        throw new Error("INvalid user data")
     }
     res.json({message:"Register User"})
 })
@@ -27,18 +47,34 @@ const registerUser = asyncHandler(async(req,res) => {
 //     POST/api/users/login
 //     public
 const loginUser =asyncHandler(async (req,res) => {
-   
-    res.json({message:"Login User"})
+    const {email,password} = req.body
+    // Check for user email
+    const user = await User.findOne({email})
+    if (user &&(await bcrypt.compare(password,user.password))){
+        res.json({
+             _id:user .id,
+            name:user.name,
+            emali:user.email,
+            token:generateToken(user._id)
+        })
+    }else{
+        res.status(400)
+        throw new Error("Invalid Credentials")
+    }
 })
-
+ 
 //     Get user data
 //     GET/api/users/me
-//     public
+//     Private
 const getMe =asyncHandler(async (req,res) => {
     res.json({message:"User data display"})
 })
-
-
+// Generate JWT
+const generateToken = (id) =>{
+    return jwt.sign({id},process.env.JWT_SECRET,{
+        expiresIn:'30d'
+    })
+}
 
 module.exports ={
     registerUser,
--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,9 +1,10 @@
 const express = require('express')
 const router =express.Router()
 const {registerUser, loginUser, getMe} = require('../controller/userController.js')
+const {protect} = require('../middlewares/authmiddleware.js')
 
 router.post('/',registerUser)
 router.post('/login',loginUser)
-router.get('/me',getMe)
+router.get('/me',protect,getMe)
 
 module.exports = router;
